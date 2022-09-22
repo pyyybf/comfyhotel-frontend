0de12eda10ffda00dc721fea4d858f786f61ae93
panyue <pyyybf@gmail.com>
2022-09-22 12:00:28 -0700

    add frame of header

---
+++ src/components/Header.js
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+import logo from '../assets/logo.svg';
+
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const Header = () => {
+    const [auth, setAuth] = React.useState(false);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const handleLogin = () => {
+
+    }
+
+    return (
+        <AppBar position="static"
+                elevation={0}
+                sx={{
+                    backgroundColor: 'white',
+                    height: '4em',
+                    borderBottom: '1px solid lightgray'
+                }}
+        >
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box sx={{flexGrow: 1}}>
+                        <img src={logo} style={{height: '3em', maxWidth: '100%'}} alt="Logo"></img>
+                    </Box>
+
+                    {auth ? (<Box sx={{flexGrow: 0}}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                                <Avatar alt="Yue Pan" src=""/>
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{mt: '45px'}}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>) : (<Button variant="outlined" onClick={handleLogin}>LOGIN</Button>)}
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+export default Header;
