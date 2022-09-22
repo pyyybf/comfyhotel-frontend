4ee5f9845fa11d12529cb27adbf67a570f20f760
panyue <pyyybf@gmail.com>
2022-09-22 13:39:09 -0700

    add user api

---
+++ src/api/user.js
@@ -0,0 +1,21 @@
+import HttpRequest from "../util/request";
+
+const api = {
+    userPre: '/api/user',
+}
+
+export const loginAPI = (data) => {
+    return HttpRequest.request({
+        url: `${api.userPre}/login`,
+        method: 'POST',
+        data
+    })
+}
+
+export const registerAPI = (data) => {
+    return HttpRequest.request({
+        url: `${api.userPre}/register`,
+        method: 'POST',
+        data
+    })
+}
