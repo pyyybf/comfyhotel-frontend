c2f4a6ea3a097d481b473be6d2d4700e45420e74
panyue <pyyybf@gmail.com>
2022-09-26 15:45:10 -0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import * as React from 'react';
 import './App.css';
+import Grid from '@mui/material/Grid';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+
 import Header from "@/components/Header";
+import Home from "@/views/home/Home";
+import MyHome from "@/views/myHome/MyHome";
 
 function App() {
     return (
-        <React.Fragment>
+        <Router>
             <Header/>
-        </React.Fragment>
+            <Grid container alignItems="center" justifyContent="center">
+                <Grid item xs={12} sm={10} md={10}>
+                    <Routes>
+                        <Route path="/" element={Home()}/>
+                        <Route path="/myHome" element={MyHome()}/>
+                    </Routes>
+                </Grid>
+            </Grid>
+        </Router>
     );
 }
 
